83995e890d8b403d07df8d91d64cf2e6763bac42
Roman Ramirez <lordpdc@hotmail.com>
2017-12-09 12:54:05 -0600

    resolve with //STUB:

--- 005_Temp/src/SecondsOfTheYear.java
+++ 005_Temp/src/SecondsOfTheYear.java
@@ -13,12 +13,12 @@ public class SecondsOfTheYear {
         // In below an incomplete version of the program. Please complete it!
         // for calculated how many second have one year
         
-        int daysInYear = 0;
-        int hoursInDay = 0;
-        int minutesInHour = 0;
-        int secondsInMinute = 0;
+        // STUB: int daysInYear = 0;
+        // STUB: int hoursInDay = 0;
+        // STUB: int minutesInHour = 0;
+        // STUB: int secondsInMinute = 0;
         
-        int secondsInYear = 0;
+        // STUB: int secondsInYear = 0;
 
         System.out.println("There are " + secondsInYear + " seconds in a year");
     }
